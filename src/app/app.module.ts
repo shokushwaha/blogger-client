53708aec25cc325ea03aa73671c8863ecdf299df
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2023-12-07 21:04:28 +0530

    create components

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsConditionComponent } from './pages/terms-condition/terms-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SubcriptionFormComponent } from './subcription-form/subcription-form.component';
+import { CommentsFormComponent } from './comments/comments-form/comments-form.component';
+import { CommentsListComponent } from './comments/comments-list/comments-list.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+
+    CategoryNavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsConditionComponent,
+    ContactUsComponent,
+    SubcriptionFormComponent,
+    CommentsFormComponent,
+    CommentsListComponent,
+    AboutUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
